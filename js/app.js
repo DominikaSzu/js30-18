4e78410589207a12a4b79e9623fc6cfddfbb4f8e
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-12 09:35:38 +0200

    Did wrong, have to do it another way

--- js/app.js
+++ js/app.js
@@ -3,7 +3,3 @@ const arrayNumbers = Array.from(numbers);
 const seconds = arrayNumbers.map(number => number.dataset.time);
 
 const secNums = seconds.map(sec => sec.replace(':', '.'));
-
-const convertedSeconds = secNums.map(secNum => parseFloat(secNum));
-
-const sum = convertedSeconds.reduce((acc, currVal) => acc + currVal);
