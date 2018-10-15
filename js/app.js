009b020b2fdfaba1e9f2bcf36bcf4f7468ad2dbb
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-15 12:32:47 +0200

    Wes's version

--- js/app.js
+++ js/app.js
@@ -1,43 +1,56 @@
 const numbers = document.querySelectorAll('[data-time]');
 const arrayNumbers = Array.from(numbers);
-const seconds = arrayNumbers.map(number => number.dataset.time);
+const seconds = 
+      arrayNumbers.map(number => number.dataset.time)
+                      .map(elem => {
+                          const [mins, secs] = elem.split(':').map(parseFloat);
+                          return (mins * 60) + secs;
+                      })
+                      .reduce((acc, val) => acc + val);
 
-//const secNums = seconds.map(sec => sec.replace(':', '.'));
+let secondsLeft = seconds;
+let hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+let minutes = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60; 
 
-const min = [];
-const sec = [];
-let reggie = /:/;
+console.log(`${hours} : ${minutes} : ${secondsLeft}`)
 
-const lala = seconds.map(lal => {
-   return lal.split(reggie);
-});
 
-lala.forEach(lal => {
-    min.push(lal[0]);
-    sec.push(lal[1]);
-});
+//const min = [];
+//const sec = [];
+//let reggie = /:/;
+//
+//const lala = seconds.map(lal => {
+//   return lal.split(reggie);
+//});
+//
+//lala.forEach(lal => {
+//    min.push(lal[0]);
+//    sec.push(lal[1]);
+//});
+//
+//function makeItNum (num) {
+//    return parseFloat(num);
+//}
+//
+//const minToNumber = min.map(num => makeItNum(num));
+//const secToNumber = sec.map(num => makeItNum(num));
+//
+//function reduceItBabe (acc, currVal) {
+//    return acc + currVal;
+//}
+//
+//const minSum = minToNumber.reduce(reduceItBabe);
+//const secSum = secToNumber.reduce(reduceItBabe);
+//
+//function sumItAll (mins, secs) {
+//    let seconds = (mins * 60) + secs;
+//    let restSec = seconds % 60;
+//    let minutes = parseInt(seconds / 60);
+//    let restMin = minutes % 60;
+//    let hours = parseInt(minutes / 60);
+//    return `${hours} : ${restMin} : ${restSec}`;
+//}
 
-function makeItNum (num) {
-    return parseFloat(num);
-}
-
-const minToNumber = min.map(num => makeItNum(num));
-const secToNumber = sec.map(num => makeItNum(num));
-
-function reduceItBabe (acc, currVal) {
-    return acc + currVal;
-}
-
-const minSum = minToNumber.reduce(reduceItBabe);
-const secSum = secToNumber.reduce(reduceItBabe);
-
-function sumItAll (mins, secs) {
-    let seconds = (mins * 60) + secs;
-    let restSec = seconds % 60;
-    let minutes = parseInt(seconds / 60);
-    let restMin = minutes % 60;
-    let hours = parseInt(minutes / 60);
-    return `${hours}:${restMin}:${restSec}`;
-}
-
-sumItAll(minSum, secSum);
+//sumItAll(minSum, secSum);
