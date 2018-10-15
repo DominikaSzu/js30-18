08d48f2f4b5bac4acbc9f4e18d5f987bb4e60adb
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-15 10:17:44 +0200

    Task done, now let's see how Wes did it

--- js/app.js
+++ js/app.js
@@ -2,4 +2,42 @@ const numbers = document.querySelectorAll('[data-time]');
 const arrayNumbers = Array.from(numbers);
 const seconds = arrayNumbers.map(number => number.dataset.time);
 
-const secNums = seconds.map(sec => sec.replace(':', '.'));
+//const secNums = seconds.map(sec => sec.replace(':', '.'));
+
+const min = [];
+const sec = [];
+let reggie = /:/;
+
+const lala = seconds.map(lal => {
+   return lal.split(reggie);
+});
+
+lala.forEach(lal => {
+    min.push(lal[0]);
+    sec.push(lal[1]);
+});
+
+function makeItNum (num) {
+    return parseFloat(num);
+}
+
+const minToNumber = min.map(num => makeItNum(num));
+const secToNumber = sec.map(num => makeItNum(num));
+
+function reduceItBabe (acc, currVal) {
+    return acc + currVal;
+}
+
+const minSum = minToNumber.reduce(reduceItBabe);
+const secSum = secToNumber.reduce(reduceItBabe);
+
+function sumItAll (mins, secs) {
+    let seconds = (mins * 60) + secs;
+    let restSec = seconds % 60;
+    let minutes = parseInt(seconds / 60);
+    let restMin = minutes % 60;
+    let hours = parseInt(minutes / 60);
+    return `${hours}:${restMin}:${restSec}`;
+}
+
+sumItAll(minSum, secSum);
